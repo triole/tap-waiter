dbcac2dfef0fe91d0ea5764e99fdaca1b152c8cb
olaf michaelis <o.mic@web.de>
2024-05-06 10:02:36 +0200

    Fix tests

--- src/index_test.go
+++ src/index_test.go
@@ -3,44 +3,49 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"path/filepath"
+	"sort"
 	"testing"
+	"time"
 )
 
 var (
-	testFolder = "../testdata"
+	tempFolder     = filepath.Join(os.TempDir(), "tyson_tap_testdata")
+	testFolder     = "../testdata"
+	dummyTestFiles []string
 )
 
+func init() {
+	dummyTestFiles = createDummyFiles()
+}
+
 func TestMakeJoinerIndex(t *testing.T) {
+	validateMakeJoinerIndex(tempFolder, "created", true, dummyTestFiles, t)
+	validateMakeJoinerIndex(tempFolder, "created", false, dummyTestFiles, t)
+	sort.Strings(dummyTestFiles)
+	validateMakeJoinerIndex(tempFolder, "lastmod", true, dummyTestFiles, t)
+	validateMakeJoinerIndex(tempFolder, "lastmod", false, dummyTestFiles, t)
 	validateMakeJoinerIndex(
-		nf("dump/yaml"), "created", true, nf("sort_validate/created_asc.json"), t,
+		nf("dump/yaml"), "size", true, loadSortJSONValidtor("size.json"), t,
 	)
 	validateMakeJoinerIndex(
-		nf("dump/yaml"), "created", false, nf("sort_validate/created_desc.json"), t,
+		nf("dump/yaml"), "size", false, loadSortJSONValidtor("size.json"), t,
 	)
 	validateMakeJoinerIndex(
-		nf("dump/yaml"), "lastmod", true, nf("sort_validate/lastmod_asc.json"), t,
+		nf("dump/yaml"), "path", true, loadSortJSONValidtor("path.json"), t,
 	)
 	validateMakeJoinerIndex(
-		nf("dump/yaml"), "lastmod", false, nf("sort_validate/lastmod_desc.json"), t,
-	)
-	validateMakeJoinerIndex(
-		nf("dump/yaml"), "size", true, nf("sort_validate/size_asc.json"), t,
-	)
-	validateMakeJoinerIndex(
-		nf("dump/yaml"), "size", false, nf("sort_validate/size_desc.json"), t,
-	)
-	validateMakeJoinerIndex(
-		nf("dump/yaml"), "path", true, nf("sort_validate/path_asc.json"), t,
-	)
-	validateMakeJoinerIndex(
-		nf("dump/yaml"), "path", false, nf("sort_validate/path_desc.json"), t,
+		nf("dump/yaml"), "path", false, loadSortJSONValidtor("path.json"), t,
 	)
 }
 
-func validateMakeJoinerIndex(folder, sortBy string, ascending bool, val string, t *testing.T) {
+func validateMakeJoinerIndex(folder, sortBy string, ascending bool, exp []string, t *testing.T) {
 	idx := makeJoinerIndex(newTestParams(folder, sortBy, ascending))
-	exp := loadJSONArr(val)
+	if !ascending {
+		exp = reverseArr(exp)
+	}
 	if !orderOK(idx, exp) {
 		order := "asc"
 		if ascending == false {
@@ -48,7 +53,7 @@ func validateMakeJoinerIndex(folder, sortBy string, ascending bool, val string, 
 		}
 		t.Errorf(
 			"sort failed: %s %s, exp: %v, got: %v",
-			sortBy, order, shortprintJI(idx), fmt.Sprintf("%v", exp),
+			sortBy, order, fmt.Sprintf("%v", exp), shortprintJI(idx),
 		)
 	}
 }
@@ -60,6 +65,13 @@ func orderOK(idx tJoinerIndex, exp []string) bool {
 		}
 	}
 	return true
+}
+
+func reverseArr(arr []string) []string {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	return arr
 }
 
 func newTestParams(folder, sortBy string, ascending bool) (p tIDXParams) {
@@ -75,7 +87,8 @@ func newTestParams(folder, sortBy string, ascending bool) (p tIDXParams) {
 	return
 }
 
-func loadJSONArr(file string) (arr []string) {
+func loadSortJSONValidtor(s string) (arr []string) {
+	file := filepath.Join(testFolder, "validate_sort", s)
 	by, _, err := readFile(file)
 	if err == nil {
 		err := json.Unmarshal(by, &arr)
@@ -93,6 +106,21 @@ func nf(s string) string {
 func shortprintJI(ji tJoinerIndex) (s string) {
 	for _, el := range ji {
 		s += fmt.Sprintf("%s ", el.Path)
+	}
+	return
+}
+
+func createDummyFiles() (arr []string) {
+	os.MkdirAll(tempFolder, os.ModePerm)
+	for i := 1; i <= 3; i++ {
+		name := filepath.Join(tempFolder, fmt.Sprintf("%03d", i)+".tmp")
+		f, err := os.Create(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		arr = append(arr, filepath.Base(name))
+		time.Sleep(time.Duration(1000) * time.Millisecond)
 	}
 	return
 }
--- src/main.go
+++ src/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	lg   = logseal.Init("debug", nil, true, false)
+	lg   = logseal.Init("info", nil, true, false)
 	conf = tConf{}
 )
 
