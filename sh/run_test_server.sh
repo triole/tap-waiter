c0a25af8c0bd1b4801194538c407d0570f7f6ec9
olaf michaelis <o.mic@web.de>
2024-05-07 10:26:36 +0200

    Update shell script

--- sh/run_test_server.sh
+++ sh/run_test_server.sh
@@ -5,4 +5,6 @@ basedir=$(echo "${scriptdir%/*}")
 ps aux | grep ".go" | grep "log-level trace" |
   awk '{print $2}' | xargs -I{} kill -15 {}
 
-r ${basedir}/testdata/conf.yaml --log-level trace
+if [[ -z "${1}" ]]; then
+  r ${basedir}/testdata/conf.yaml --log-level trace
+fi
